a3587dd67ddb8ed2b716a6fec41dd4c8f8037258
PJ Fanning <pjfanning@users.noreply.github.com>
2022-04-11 15:00:26 +0200

    Update DataFormatter.java

--- poi/src/main/java/org/apache/poi/ss/usermodel/DataFormatter.java
+++ poi/src/main/java/org/apache/poi/ss/usermodel/DataFormatter.java
@@ -950,14 +950,7 @@ public class DataFormatter {
         if (numberFormat == null) {
             return Double.toString(d);
         }
-        String formatted;
-        try {
-            //see https://github.com/apache/poi/pull/321 -- but this sometimes fails as Double.toString
-            //can produce strings that can't be parsed by BigDecimal
-            formatted = numberFormat.format(new BigDecimal(Double.toString(d)));
-        } catch (NumberFormatException nfe) {
-            formatted = numberFormat.format(d);
-        }
+        String formatted = numberFormat.format(new BigDecimal(NumberToTextConverter.toText(d)));
         return formatted.replaceFirst("E(\\d)", "E+$1"); // to match Excel's E-notation
     }
 
